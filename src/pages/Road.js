df95d6129342813e12ebe85bdf6497cb05d93bc5
Liketodev2 <liketodev2@gmail.com>
2021-06-03 18:20:11 +0400

    Road

--- src/pages/Road.js
+++ src/pages/Road.js
@@ -7,41 +7,23 @@ import RoadItem from '../components/roadItem';
 function Road(){
 
     const [item , setitem] = useState([])
-    const [visible, setVisible] = useState(3)
     const container = useRef(0)
-
-    // const [scroll , setScroll] = useState(0)
     
-    const readMore = () => {
-        setVisible((prevValue) => prevValue +3)
-    }
-    
-    useEffect(() => {
-        let scroll      = window.scrollY;
-        // setScroll(window.scrollY)
-        alert(scroll)
-        // if(scroll <= ((container.current.offsetHeight) - 200)){
-        //     readMore()
-        // }
-        // console.log(container.current.offsetHeight)
-    })
-
     useEffect(() => {
 	    fetch('https://jsonplaceholder.typicode.com/posts')
         	.then((res) => res.json())
-        	.then((data) => setitem(data))
+        	.then((item) => setitem(item))
     }, [])
   
     return(
         <>
         <div className='Road'  ref={container}>
             <div className='Road__container'>
-                {/* <div className='Road__columne'/> */}
+                    <span className='Road__box__line'/>
                 <div className='Road__box'>
-                    {item.slice(0, visible).map((item) => (
+                    {item.map((item) => (
                         <RoadItem title={item.title} text={item.body} key={item.id}/>
 	                ))}
-                    <button className='add3item' onClick={readMore}>ADD new 3 item</button>
                 </div>
             </div>
             <MailInp/>
