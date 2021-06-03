e71c408d6eaec112ec3b4f09da7b751427a7f8a8
Jacob Walls <liketodev2@gmail.com>
2021-06-03 12:53:42 +0400

    test git

--- src/components/roadItem.js
+++ src/components/roadItem.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import './../scss/road.scss'
-
+import BookmarkSimple from './../assets/images/BookmarkSimple.png'
+import Check from './../assets/images/Check.png'
+import Clock from './../assets/images/Clock.png'
 
 function RoadItem(props){
 
@@ -8,13 +10,21 @@ function RoadItem(props){
 
     return(
         <div className='RoadItem'>
+            <div  className='RoadItem__circle__container'>
+                <div  className='RoadItem__circle'>
+                
+                </div>
+            </div>
             <div className="RoadItem__content">
                 <p className="RoadItem__title">{props.title}</p>
                 <p className="RoadItem__description">{props.text}</p>
             </div>
             <div className="RoadItem__status">
-                <div>
-                    <div className="RoadItem__icon-circle" />
+                <div className='RoadItem__icon__block'>
+                        <img src={BookmarkSimple} className='RoadItem__icon' alt='BookmarkSimple-icon'/>
+                        {/* <img src={Check} className='RoadItem__icon' alt='Check-icon'/>
+                        <img src={Clock} className='RoadItem__icon' alt='Clock-icon'/> */}
+                        <p className='RoadItem__status'>Done</p>
                 </div>
                 <a href="/">Viwe</a>
             </div>
--- src/pages/Road.js
+++ src/pages/Road.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react'
+import React, {useState, useEffect, useRef} from 'react'
 import './../scss/road.scss'
 import Footer from '../components/FooterFAQ';
 import MailInp from '../components/mailInp';
@@ -8,10 +8,23 @@ function Road(){
 
     const [item , setitem] = useState([])
     const [visible, setVisible] = useState(3)
+    const container = useRef(0)
 
+    // const [scroll , setScroll] = useState(0)
+    
     const readMore = () => {
         setVisible((prevValue) => prevValue +3)
     }
+    
+    useEffect(() => {
+        let scroll      = window.scrollY;
+        // setScroll(window.scrollY)
+        alert(scroll)
+        // if(scroll <= ((container.current.offsetHeight) - 200)){
+        //     readMore()
+        // }
+        // console.log(container.current.offsetHeight)
+    })
 
     useEffect(() => {
 	    fetch('https://jsonplaceholder.typicode.com/posts')
@@ -21,12 +34,12 @@ function Road(){
   
     return(
         <>
-        <div className='Road'>
+        <div className='Road'  ref={container}>
             <div className='Road__container'>
                 {/* <div className='Road__columne'/> */}
                 <div className='Road__box'>
                     {item.slice(0, visible).map((item) => (
-                        <RoadItem title={item.title} text={item.body}/>
+                        <RoadItem title={item.title} text={item.body} key={item.id}/>
 	                ))}
                     <button className='add3item' onClick={readMore}>ADD new 3 item</button>
                 </div>
