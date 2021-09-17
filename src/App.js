d4ca04ef36c085f74787e1facec7f1ccc2a20fbd
LiquiDAO Developers <74860369+liquidao-dev@users.noreply.github.com>
2021-09-17 14:57:03 +0000

    homepage

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <div className="page" id="page">
         <Navbar />
         <div id="routerContent">
